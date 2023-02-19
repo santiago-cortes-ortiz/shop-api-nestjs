5d9d7cb6eaaf35607183e6253726a04c905fdd47
jeiss <jeissoncortes02@outlook.com>
2023-02-18 19:39:17 -0500

    controladores de los casos de uso

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './controllers/products.controller';
 import { CategoriesController } from './controllers/categories.controller';
+import { BrandsController } from './controllers/brands.controller';
+import { OrdersController } from './controllers/orders/orders.controller';
+import { OrdersController } from './controllers/orders.controller';
+import { UsersController } from './controllers/users.controller';
+import { CustomersController } from './controllers/customers.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController],
+  controllers: [AppController, ProductsController, CategoriesController, BrandsController, OrdersController, UsersController, CustomersController],
   providers: [AppService],
 })
 export class AppModule {}

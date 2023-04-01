54250dca21d25b29851d8b497d06b5d1a826681e
jeiss <jeissoncortes02@outlook.com>
2023-04-01 16:59:02 -0500

    se aagrega validadores a nest de mis dtos

--- src/dtos/product.dto.ts
+++ src/dtos/product.dto.ts
@@ -1,14 +1,40 @@
+import {
+  IsString,
+  IsNumber,
+  IsNotEmpty,
+  IsUrl,
+  IsPositive,
+} from 'class-validator';
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly price: number;
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
   readonly stock: number;
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
 }
 export class UpdateProductDto {
+  @IsString()
   readonly name?: string;
+  @IsString()
   readonly description?: string;
+  @IsNumber()
+  @IsPositive()
   readonly price?: number;
+  @IsNumber()
+  @IsPositive()
   readonly stock?: number;
+  @IsUrl()
   readonly image?: string;
 }
